d51b886d22fe9f78137ab227e3cc954beee545d2
Sanjay Solanki <sanjay.solanki.17.om@gmail.com>
2024-12-02 20:24:32 +0530

    Scroll Set For User accounts scroll effect done for now.

--- client/src/home/LeftPart/User.jsx
+++ client/src/home/LeftPart/User.jsx
@@ -1,24 +1,35 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import UserAccounts from "./UserAccounts";
+import { ScrollArea } from "@/components/ui/scroll-area"
+import './ScrollArea.css'
 
 const User = () => {
   return (
     <>
-      <div>
-        <h1 className="text-base font-bold bg-[#f2efe8] text-[#2a3246] px-6 py-1">
+      <div className="h-screen flex flex-col">
+        <h1 className="text-base font-bold bg-[#f2efe8] text-[#2a3246] px-6 py-1 mb-6">
           Messages
         </h1>
-        <div className="text-black flex space-x-4 px-6 py-6 place-items-center">
-        <Avatar className="w-14 h-14" >
-          <AvatarImage src="https://github.com/shadcn.png" />
-          <AvatarFallback className="text-black">CN</AvatarFallback>
-        </Avatar>
-        <div>
-          <h1 className="font-semibold">Sanjay Solanki</h1>
-          <span className="text-xs">Sanjay@Dev.com</span>
-        </div>
-        </div>
+        {/* w-full rounded-md border p-4 overflow-y-auto */}
+        <ScrollArea className="custom-scroll-area ">
+          <UserAccounts/>
+          <UserAccounts/>
+          <UserAccounts/>
+          <UserAccounts/>
+          <UserAccounts/>
+          <UserAccounts/>
+          <UserAccounts/>
+          <UserAccounts/>
+          <UserAccounts/>
+          <UserAccounts/>
+          <UserAccounts/>
+          <UserAccounts/>
+          <UserAccounts/>
+          <UserAccounts/>
+          <UserAccounts/>
+        </ScrollArea>
         </div>
     </>
   );
---
+++ client/src/home/LeftPart/UserAccounts.jsx
@@ -0,0 +1,23 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
+
+const UserAccounts = () => {
+  return (
+    <>
+    <div className="text-black flex space-x-4 px-6 py-4 place-items-center hover:bg-[#f2efe8] duration-500 cursor-pointer">
+        <Avatar className="w-12 h-12" >
+          <AvatarImage src="https://github.com/shadcn.png" />
+          <AvatarFallback className="text-black">CN</AvatarFallback>
+        </Avatar>
+        <div>
+          <h1 className="font-semibold">Sanjay Solanki</h1>
+          <span className="text-xs">Sanjay@Dev.com</span>
+        </div>
+        </div>
+    </>
+  )
+}
+
+export default UserAccounts
