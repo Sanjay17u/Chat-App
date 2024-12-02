6a79ff56fa4526816b1dbe480b5ef0a15f36b553
Sanjay Solanki <sanjay.solanki.17.om@gmail.com>
2024-12-02 18:43:28 +0530

    Search UI of Left part is done for Now.

--- client/src/home/LeftPart/Search.jsx
+++ client/src/home/LeftPart/Search.jsx
@@ -1,14 +1,18 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React from "react";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { SearchIcon } from 'lucide-react'
 
 const Search = () => {
   return (
-    <div>
-      <h1 className='text-2xl font-semibold'>
-        Search
-      </h1>
-    </div>
-  )
-}
+    <>
+      <div className="flex w-full max-w-sm items-center space-x-2 px-6 py-4 place-items-center sm:max-w-md md:max-w-lg lg:max-w-xl">
+        <Input type="text" placeholder="Search" className="font-bold shadow-md" />
+        <Button type="submit" className="rounded-full hover:shadow-sm"><SearchIcon/></Button>
+      </div>
+    </>
+  );
+};
 
-export default Search
+export default Search;
