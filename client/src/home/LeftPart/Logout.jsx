81ecc8efe01eb2282ca303b5c4f73f4aa7127903
Sanjay Solanki <sanjay.solanki.17.om@gmail.com>
2024-12-02 21:04:22 +0530

    add some hover text at logout component Button.

--- client/src/home/LeftPart/Logout.jsx
+++ client/src/home/LeftPart/Logout.jsx
@@ -2,15 +2,34 @@
 import React from "react";
 import { Button } from "@/components/ui/button";
 import { LogOut, SearchIcon } from 'lucide-react'
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/hover-card"
 
 const Logout = () => {
   return (
     <>
       <div className="flex w-full max-w-sm items-center space-x-2 px-6 py-4 place-items-center sm:max-w-md md:max-w-lg lg:max-w-xl">
-        <Button type="submit" className="rounded-full hover:shadow-sm ">
-          <LogOut className="transform rotate-180" strokeWidth={3}/>
-        </Button>
-      </div>
+      <HoverCard>
+        <HoverCardTrigger>
+          <Button
+            type="submit"
+            className="relative rounded-full hover:shadow-sm px-4 py-2 border-cyan-50"
+          >
+            <LogOut className="transform rotate-180" strokeWidth={3} />
+
+            <span className="absolute left-0 inset-y-0 opacity-0 group-hover:opacity-100 transition-opacity duration-200">
+              Logout
+            </span>
+          </Button>
+        </HoverCardTrigger>
+        <HoverCardContent className="text-sm font-bold w-fit text-[#ff0055]">
+          Logout Now.!
+        </HoverCardContent>
+      </HoverCard>
+    </div>
     </>
   );
 };
