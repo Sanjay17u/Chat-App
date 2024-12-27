8256465de4cfd9fe2e5f3186dfbd7fa9b95cdd99
Sanjay Solanki <sanjay.solanki.17.om@gmail.com>
2024-12-27 21:22:08 +0530

    GetAllUsers controller Work Still Going On.

--- server/controller/user.controller.js
+++ server/controller/user.controller.js
@@ -95,3 +95,13 @@ export const logout = async (req, res) => {
         })
     }
 }
+
+export const allUsers = async (req, res) => {
+    try {
+        const loggedInUser = req.user._id;
+        const filteredUsers = await User.find({ _id: { $ne: loggedInUser } }).select("-password")
+        res.status(201).json(filteredUsers)
+    } catch (error) {
+        console.log('Error While Getting All Users : ', error)        
+    }
+} 
