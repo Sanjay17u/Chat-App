a6fb35213dc4a28fd767c6ef1033d3b8cbbdde28
Sanjay Solanki <sanjay.solanki.17.om@gmail.com>
2025-01-02 21:20:40 +0530

    setup Tailwind css in The project frontend

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html", // Vite's entry point
+    "./src/**/*.{js,jsx,ts,tsx}", // All your React files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
