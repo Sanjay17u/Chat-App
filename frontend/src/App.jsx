b50caa425fc30da78f555f3f35a514b49fe6557c
Sanjay Solanki <sanjay.solanki.17.om@gmail.com>
2025-01-02 21:28:30 +0530

    daisy ui Setup we r ready to make front Part of our app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,20 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
-
+import React from "react";
 
 const App = () => {
-  return(
+  return (
     <>
-      <div className='text-red-600'>
-        Hello
+      <div>
+        <button className="btn">Button</button>
+        <button className="btn btn-neutral">Neutral</button>
+        <button className="btn btn-primary">Primary</button>
+        <button className="btn btn-secondary">Secondary</button>
+        <button className="btn btn-accent">Accent</button>
+        <button className="btn btn-ghost">Ghost</button>
+        <button className="btn btn-link">Link</button>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
