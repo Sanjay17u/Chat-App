c9711af2c5cba12161c7f3a7c9f6457c4e91e650
Sanjay Solanki <sanjay.solanki.17.om@gmail.com>
2025-01-03 21:24:38 +0530

    Making Login and Signup Frontend Page.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,26 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
+import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import SettingPage from "./pages/SettingPage";
+import ProfilePage from "./pages/ProfilePage";
 
 const App = () => {
   return (
     <>
-      <div>
-        <button className="btn">Button</button>
-        <button className="btn btn-neutral">Neutral</button>
-        <button className="btn btn-primary">Primary</button>
-        <button className="btn btn-secondary">Secondary</button>
-        <button className="btn btn-accent">Accent</button>
-        <button className="btn btn-ghost">Ghost</button>
-        <button className="btn btn-link">Link</button>
-      </div>
+      <Navbar/>
+
+      <Routes>
+        <Route path="/" element={ <HomePage/> } />
+        <Route path="/signup" element={ <Signup/> } />
+        <Route path="/login" element={ <Login/> } />
+        <Route path="/settings" element={ <SettingPage/> } />
+        <Route path="/profile" element={ <ProfilePage/> } />
+      </Routes>
+
     </>
   );
 };
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,6 +4,8 @@ export default {
   content: [
     "./index.html", // Vite's entry point
     "./src/**/*.{js,jsx,ts,tsx}", // All your React files
+    "./src/component/**/*.{js,jsx,ts,tsx}",
+    "./src/pages/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
